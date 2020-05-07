3112723ba3866677d8fff313b0d21ba7acec5f44
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-07 18:46:28 +0100

    change to freq

--- run_freq.bat
+++ run_freq.bat
@@ -1,4 +1,4 @@
 setlocal
 cd /d %~dp0
 @echo off
-cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & python run_week.py"
+cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & python run_freq.py"
