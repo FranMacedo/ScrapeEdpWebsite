ffd8140c2e5bcced257a7a2fafb897555321ba89
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-08 20:41:25 +0100

    exit cmd after

--- run_freq.bat
+++ run_freq.bat
@@ -1,4 +1,4 @@
 setlocal
 cd /d %~dp0
 @echo off
-cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & python run_freq.py"
+cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & python run_freq.py & exit"
