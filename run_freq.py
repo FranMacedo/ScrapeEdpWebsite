968a3b5f96b61a3db933f3689be3b7d7e479b28b
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-19 09:36:33 +0100

    daily update

--- run_freq.py
+++ run_freq.py
@@ -5,20 +5,18 @@ from dateutil.relativedelta import relativedelta
 
 today_date = dt.now().date()
 
-print('lalala')
-import time
-time.sleep(3)
-# if today_date.day < 10:
-# 	date_list = [today_date - relativedelta(months=1), today_date]
-# else:
-# 	date_list = [today_date]
 
-# if today_date == dt(2020, 5, 19).date():
-# 	date_list = ['2020-01', '2020-02', '2020-03', '2020-04']
-# 	multi_robot(gestao='CML', date_list=date_list, replace=True)
-# 	from robot_info import get_info
-# 	get_info(gestao='cml', get_new=True)
-# else:
-# 	multi_robot(gestao='EGEAC', date_list=date_list, replace=True)
-# 	multi_robot(gestao='SCML', date_list=date_list, replace=True)
-# 	multi_robot(gestao='CML', date_list=date_list, replace=True)
+if today_date.day < 10:
+	date_list = [today_date - relativedelta(months=1), today_date]
+else:
+	date_list = [today_date]
+
+if today_date == dt(2020, 5, 19).date():
+	date_list = ['2020-01', '2020-02', '2020-03', '2020-04']
+	multi_robot(gestao='CML', date_list=date_list, replace=True)
+	from robot_info import get_info
+	get_info(gestao='cml', get_new=True)
+else:
+	multi_robot(gestao='EGEAC', date_list=date_list, replace=True)
+	multi_robot(gestao='SCML', date_list=date_list, replace=True)
+	multi_robot(gestao='CML', date_list=date_list, replace=True)
