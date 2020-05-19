0e182e74977379dc75909042335d0c30d4cdf79e
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-19 09:41:21 +0100

    +lalalalalalala+

--- git_update.bat
+++ git_update.bat
@@ -1,5 +1,5 @@
 setlocal
 cd /d %~dp0
 @echo off
-set /p mes="Enter commit message: "
-cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & git add . & git commit -m "+%id%+" & git push origin master & exit"
+set /p mess="Enter commit message: "
+cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & git add . & git commit -m "+%mess%+" & git push origin master & exit"
