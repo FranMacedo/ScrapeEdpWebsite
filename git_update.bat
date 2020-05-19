ce0da50e04ede25e8e82ebab93739e5b26020e9f
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-19 09:36:51 +0100

    daily update

--- git_update.bat
+++ git_update.bat
@@ -1,4 +1,5 @@
 setlocal
 cd /d %~dp0
 @echo off
+set /p id="Enter ID: "
 cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & git add . & git commit -m "daily update" & git push origin master & exit"
