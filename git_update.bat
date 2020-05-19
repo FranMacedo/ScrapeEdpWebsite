743c3262eaff2030e8a6a1eb1c31491db24ae3ad
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-19 09:40:39 +0100

    ++

--- git_update.bat
+++ git_update.bat
@@ -1,5 +1,5 @@
 setlocal
 cd /d %~dp0
 @echo off
-set /p id="Enter ID: "
-cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & git add . & git commit -m "daily update" & git push origin master & exit"
+set /p mes="Enter commit message: "
+cmd /k "cd /d venv\Scripts & activate & cd.. & cd.. & git add . & git commit -m "+%id%+" & git push origin master & exit"
