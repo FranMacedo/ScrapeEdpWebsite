5e717e6469465ba31b0d494a6c714bd3ce29795a
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-19 09:47:15 +0100

    auto git update

--- git_update.bat
+++ git_update.bat
@@ -2,4 +2,4 @@ setlocal
 cd /d %~dp0
 @echo off
 set /p mess="Enter commit message: "
-cmd /k "git add . & git commit -m "%mess%" & git push origin master & pause"
+cmd /k "git add . & git commit -m "%mess%" & git push origin master & exit"
