47e29b47a9141c0a2a1b72664884749b6d12d753
FranMacedo <franciscomacedo@lisboaenova.org>
2020-05-28 16:42:56 +0100

    update info; download early 2020

--- run_freq.py
+++ run_freq.py
@@ -11,11 +11,17 @@ if today_date.day < 10:
 else:
 	date_list = [today_date]
 
+
 if today_date == dt(2020, 5, 28).date():
-	date_list = ['2020-01', '2020-02', '2020-03', '2020-04']
-	# multi_robot(gestao='CML', date_list=date_list, replace=True)
 	from robot_info import get_info
 	get_info(gestao='cml', get_new=True)
+
+
+if today_date == dt(2020, 5, 29).date():
+	date_list = ['2020-01', '2020-02', '2020-03', '2020-04']
+	multi_robot(gestao='CML', date_list=date_list, replace=True)
+	# from robot_info import get_info
+	# get_info(gestao='cml', get_new=True)
 else:
 	multi_robot(gestao='EGEAC', date_list=date_list, replace=True)
 	multi_robot(gestao='SCML', date_list=date_list, replace=True)
