453ae1f087039642a86f484efdd58822ee40033d
FranMacedo <franciscomacedo@lisboaenova.org>
2020-06-15 17:37:37 +0100

    daily update

--- run_info.bat
+++ run_info.bat
@@ -3,4 +3,4 @@ cd /d %~dp0
 @echo off
 cmd /c "powershell -ExecutionPolicy Bypass -File update_chromedriver.ps1 & cd /d venv\Scripts & activate & cd.. & cd.. & python run_info.py"
 cmd /c "git add . & git commit -m "daily update" & git pull origin master & git push origin master & exit"
-cmd /c exit -1073741510
+cmd /c exit -1073741510 "Y"
