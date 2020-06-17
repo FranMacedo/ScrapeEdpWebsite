4bc4481e984305eba33a682c130f829e2409a490
FranMacedo <franciscomacedo@lisbaenova.org>
2020-06-17 17:29:25 +0100

    INFO update

--- py_scripts/run_info.py
+++ py_scripts/run_info.py
@@ -1,5 +1,5 @@
 from func.robot_info import get_info
 import sys
 
-get_info(gestao='CML', get_new=True, only_active=True, no_BTN=True)
+get_info(gestao='EGEAC', get_new=True, only_active=True, no_BTN=True)
 sys.exit()
