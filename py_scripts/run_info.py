cbbacfdf213bfc73f99a7085faaae7433191e2ea
FranMacedo <franciscomacedo@lisbaenova.org>
2020-06-17 18:48:27 +0100

    INFO update

--- py_scripts/run_info.py
+++ py_scripts/run_info.py
@@ -1,5 +1,5 @@
 from func.robot_info import get_info
 import sys
 
-get_info(gestao='CML', get_new=True, only_active=True, no_BTN=False)
+get_info(gestao='SCML', get_new=True, only_active=True, no_BTN=True)
 
