ac243798f06d3949081ceb0bd41a902fbcbe5be4
FranMacedo <franciscomacedo@lisboaenova.org>
2020-07-03 09:37:38 +0100

    daily update

--- run_gui.bat
+++ run_gui.bat
@@ -1,4 +1,4 @@
 setlocal
 cd /d %~dp0
 @echo off
-cmd /k "powershell -ExecutionPolicy Bypass -File update_chromedriver.ps1 & cd /d venv\Scripts & activate & cd.. & cd.. & python py_scripts/run_gui.py & exit"
+cmd /k "powershell -ExecutionPolicy Bypass -File update_chromedriver.ps1 & cd /d venv\Scripts & activate & cd.. & cd.. & python py_scripts/run_gui.py & pause"
